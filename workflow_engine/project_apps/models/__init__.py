3fe669d82e6302a355cedee34a0ec32b4dcdbc11
giyong-choi <rldyd888@naver.com>
2024-02-24 01:23:29 +0900

    feat : ZRWC-93 : 스케줄링 DB를 확정한다.

--- workflow_engine/project_apps/models/__init__.py
+++ workflow_engine/project_apps/models/__init__.py
@@ -1,3 +1,4 @@
 from project_apps.models.workflow import Workflow
 from project_apps.models.job import Job
 from project_apps.models.history import History
+from project_apps.models.scheduling import Scheduling
