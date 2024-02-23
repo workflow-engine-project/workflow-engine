45d638230b2d40b6e4ce7cdce3d9e56a662df743
minsuhaha <haminsu5@gmail.com>
2024-02-23 11:38:57 +0900

    feat : ZRWC-86 : job task가 실패처리되었을 경우 해당 워크플로우의 실행중인 job task들도 모두 종료하는 로직 추가구현.
    - 동일한 워크플로우 하나의 job이 실패 처리되었을 시에, 다른 동작중인 모든 job들의 실행을 멈추고 워크플로우를 종료하는 로직을 구현하도록 하는 로직 구현.

--- workflow_engine/project_apps/service/workflow_manage.py
+++ workflow_engine/project_apps/service/workflow_manage.py
@@ -1,6 +1,7 @@
 import orjson as json
 
-from project_apps.constants import HISTORY_STATUS_FAIL, HISTORY_STATUS_SUCCESS, JOB_STATUS_SUCCESS
+from project_apps.constants import HISTORY_STATUS_FAIL, HISTORY_STATUS_SUCCESS, JOB_STATUS_SUCCESS, WORKFLOW_STATUS_FAIL, WORKFLOW_STATUS_SUCCESS
+from project_apps.engine.job_terminate import job_terminate
 from project_apps.engine.tasks_manager import job_dependency
 from project_apps.models.cache import Cache
 from project_apps.repository.history_repository import HistoryRepository
@@ -34,12 +35,43 @@ class WorkflowManager:
         특정 작업의 상태를 업데이트하고, 변경된 워크플로우 데이터를 캐시에 저장.
         '''
         workflow_data = json.loads(self.cache.get(workflow_uuid))
-        for job in workflow_data:
-            if job['uuid'] == str(job_uuid):
-                job['result'] = status
-                break
-        
-        self.cache.set(workflow_uuid, json.dumps(workflow_data))
+
+        workflow_status = self.check_workflow_status(workflow_uuid)
+        if workflow_status == WORKFLOW_STATUS_FAIL:
+            for job in workflow_data:
+                if job['uuid'] == str(job_uuid):
+                    job['result'] = status
+                    print(f"{job['uuid'], job['result']}")
+                    break
+            self.cache.set(workflow_uuid, json.dumps(workflow_data))
+            return False
+        else:
+            for job in workflow_data:
+                if job['uuid'] == str(job_uuid):
+                    job['result'] = status
+                    print(f"{job['uuid'], job['result']}")
+                    break
+            self.cache.set(workflow_uuid, json.dumps(workflow_data))
+            return True
+
+    @with_lock
+    def update_workflow_status(self, workflow_uuid, status):
+        '''
+        워크플로우의 상태를 업데이트. 만약 상태가 실패로 업데이트된 경우, 
+		실행 중인 모든 도커 컨테이너를 종료
+        '''
+        self.cache.set(f"{workflow_uuid}_status", status)
+        if status == WORKFLOW_STATUS_FAIL:
+            running_containers = self.cache.get(f"{workflow_uuid}_running_containers")
+            if running_containers:
+                for container_id in running_containers:
+                    job_terminate.apply_async(args=[container_id])
+    
+    def check_workflow_status(self, workflow_uuid):
+        '''
+        워크플로우의 상태를 확인.
+        '''
+        return self.cache.get(f"{workflow_uuid}_status")
 
     @with_lock
     def handle_success(self, job_data, workflow_uuid, history_uuid, history_repo):
@@ -67,10 +99,10 @@ class WorkflowManager:
     def handle_failure(self, history_uuid, workflow_uuid, history_repo):
         '''
         작업 실행 실패 시 처리 로직을 수행.
-        관련 히스토리를 업데이트하고, 워크플로우 데이터를 캐시에서 삭제.
+        워크플로우 실패 상태를 설정하고, 관련 히스토리를 업데이트.
         '''
+        self.update_workflow_status(workflow_uuid, WORKFLOW_STATUS_FAIL)
         history_repo.update_history_status(history_uuid, HISTORY_STATUS_FAIL)
-        self.cache.delete(workflow_uuid)
 
     def check_workflow_completion(self, workflow_uuid, history_uuid, history_repo):
         '''
@@ -85,5 +117,24 @@ class WorkflowManager:
                 break
 
         if completed:
+            self.update_workflow_status(workflow_uuid, WORKFLOW_STATUS_SUCCESS)
             history_repo.update_history_status(history_uuid, HISTORY_STATUS_SUCCESS)
-            self.cache.delete(workflow_uuid)
+
+    @with_lock
+    def add_container_to_running_list(self, workflow_uuid, container_id):
+        '''
+        실행 중인 도커 컨테이너의 ID를 워크플로우의 실행중인 컨테이너 목록에 추가.
+        '''
+        running_containers = self.cache.get(f"{workflow_uuid}_running_containers")
+        running_containers.append(container_id)
+        self.cache.set(f"{workflow_uuid}_running_containers", running_containers)
+
+    @with_lock
+    def remove_container_from_running_list(self, workflow_uuid, container_id):
+        '''
+        워크플로우의 실행 중인 컨테이너 목록에서 특정 컨테이너의 ID를 제거.
+        '''
+        running_containers = self.cache.get(f"{workflow_uuid}_running_containers")
+        if container_id in running_containers:
+            running_containers.remove(container_id)
+            self.cache.set(f"{workflow_uuid}_running_containers", running_containers)
