7d1128d09d7c39effb074d06e1872bb0f8be0937
hwan3526 <hwan3526@gmail.com>
2024-03-08 16:24:41 +0900

    refactor : ZRWC-126 : URL 관련 리팩터링을 진행한다.

--- workflow_engine/project_apps/api/urls.py
+++ workflow_engine/project_apps/api/urls.py
@@ -4,12 +4,10 @@ from project_apps.api.views import *
 
 urlpatterns = [
     path('workflow', WorkflowAPIView.as_view()),
-    path('workflow/<uuid:workflow_uuid>', WorkflowAPIView.as_view()),
-    path('workflow/list', WorkflowListReadAPIView.as_view()),
-    path('workflow/execute/<uuid:workflow_uuid>', WorkflowExecuteAPIView.as_view()),
-    path('workflow/scheduling', SchedulingAPIView.as_view()),
-    path('workflow/scheduling/<uuid:scheduling_uuid>', SchedulingAPIView.as_view()),
-    path('workflow/scheduling/list', SchedulingListReadAPIView.as_view()),
-    path('workflow/scheduling/list/<uuid:workflow_uuid>', WorkflowSchedulingListReadAPIView.as_view()),
-    path('workflow/scheduling/execute/<uuid:scheduling_uuid>', SchedulingExecuteAPIView.as_view()),
+    path('workflow/<uuid:workflow_uuid>', WorkflowUUIDAPIView.as_view()),
+    path('workflow/<uuid:workflow_uuid>/execute', WorkflowExecuteAPIView.as_view()),
+    path('scheduling', SchedulingAPIView.as_view()),
+    path('scheduling/<uuid:scheduling_uuid>', SchedulingUUIDAPIView.as_view()),
+    path('scheduling/workflow/<uuid:workflow_uuid>', SchedulingWorkflowAPIView.as_view()),
+    path('scheduling/<uuid:scheduling_uuid>/execute', SchedulingExecuteAPIView.as_view()),
 ]
